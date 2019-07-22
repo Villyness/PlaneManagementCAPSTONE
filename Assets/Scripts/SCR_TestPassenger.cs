bdd5670d40cbef2eb029931a72d6a75795dc1a56
Venus <inkyinked@gmail.com>
2019-07-22 13:35:50 +1000

    Finished basic passenger needs; working on removing items when given to the passenger.

--- Assets/Scripts/SCR_TestPassenger.cs
+++ Assets/Scripts/SCR_TestPassenger.cs
@@ -1,21 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SCR_TestPassenger : MonoBehaviour
 {
-    public SCR_InteractableItem currentNeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentNeed = new SCR_InteractableItem();
-        currentNeed.Name = "Apple";
-        Debug.Log(currentNeed.Name);
-    }
+    #region PassengerNeeds
+    //public SCR_InteractableItem currentNeed;
+    public string CurrentNeed;
+    public string[] ListOfNeeds;
+    #endregion
+
+    public event Action Interacted;
 
     // Update is called once per frame
     void Update()
     {
-        
+        #region PassengerNeedsChange
+        /*if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            CurrentNeed = ListOfNeeds[Random.Range(0, 3)];
+        //print(ListOfNeeds[Random.Range(0, 3)])*/
+        #endregion
+    }
+
+    public void OnMouseDown()
+    {
+        if (Interacted != null)
+            Interacted();
+        //Debug.Log("Clicked");
     }
 }
