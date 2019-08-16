73d8379a5ef21567e2cd6e0b9368168fe313d8e3
Jing Hu <1013263@student.sae.edu.au>
2019-08-16 23:54:14 +1000

    new camera

---
+++ Assets/Scripts/CameraScript_Follow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraScript_Follow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+    }
+}
