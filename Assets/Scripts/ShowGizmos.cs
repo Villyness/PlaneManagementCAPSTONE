527f876bca9caa8cb7913fec6e1d716800c1668e
Jing <1013263@student.sae.edu.au>
2019-10-07 14:46:16 +1100

    camera clamping test

---
+++ Assets/Scripts/ShowGizmos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowGizmos : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, GetComponent<Collider>().bounds.size);
+#endif
+    }
+}
