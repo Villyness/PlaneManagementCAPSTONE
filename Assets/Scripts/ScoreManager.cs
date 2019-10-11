a53896f3f2acef6d0ddc7e38107807eb9c414e07
Venus <inkyinked@gmail.com>
2019-10-11 15:51:03 +1100

    Added stars that turn themselves yellow based on the score.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+using Image = UnityEngine.UIElements.Image;
 
 public class ScoreManager : MonoBehaviour
 {
     //TODO: <pr> = set to private after testing
-    public int level = 1;    // Setting it to 1 so that it won't get any out of range errors
+    public int level = 0;    // Setting it to 1 so that it won't get any out of range errors
     public int score;
 
     //for each level go through and set individually
@@ -19,9 +20,9 @@ public class ScoreManager : MonoBehaviour
     public int[] bestScore = {200,250,300,350,400};
 
     //stars for winning <pr>
-    public GameObject passStar;
-    public GameObject goodStar;
-    public GameObject bestStar;
+    public UnityEngine.UI.Image passStar;
+    public UnityEngine.UI.Image goodStar;
+    public UnityEngine.UI.Image bestStar;
 
     // <pr>
     public Material matPass;
@@ -58,7 +59,7 @@ public class ScoreManager : MonoBehaviour
         //ScoreReset();
     }
 
-    void ScoreReset()
+    /*void ScoreReset()
     {
         score = 0;
         //this makes it a pop up rather than a seperate screen
@@ -69,7 +70,7 @@ public class ScoreManager : MonoBehaviour
         bestStar.GetComponent<Renderer>().enabled = false;
         goodStar.GetComponent<Renderer>().enabled = false;
         passStar.GetComponent<Renderer>().enabled = false;
-    }
+    }*/
 
     private void Update()
     {
@@ -81,34 +82,39 @@ public class ScoreManager : MonoBehaviour
 
     void LevelEnd()
     {
+        //passStar.color = Color.yellow;
+        
         /*endScreen.GetComponent<Renderer>().enabled = true;
         bestStar.GetComponent<Renderer>().enabled = true;
         goodStar.GetComponent<Renderer>().enabled = true;
-        passStar.GetComponent<Renderer>().enabled = true;
-        
+        passStar.GetComponent<Renderer>().enabled = true;*/
+        Debug.Log(passScore[level]);
         //check score, have gameobjects for each.
         if (score >= bestScore[level])
         {
-            passed = true;
-            bestStar.GetComponent<Renderer>().material = matPass;
+            //passed = true;
+            //bestStar.GetComponent<Renderer>().material = matPass;
             //best score
+            bestStar.color = Color.yellow;
         }
         if (score >= goodScore[level])
         {
-            passed = true;
-            
-            goodStar.GetComponent<Renderer>().material = matPass;
+            //passed = true;
+            goodStar.color = Color.yellow;
+            //goodStar.GetComponent<Renderer>().material = matPass;
             //good score
         }
         if (score >= passScore[level])
         {
+            passStar.color = Color.yellow;
+            
             passed = true;
-            passStar.GetComponent<Renderer>().material = matPass;
+            //passStar.GetComponent<Renderer>().material = matPass;
             //passed
         }
         else
         {
-            
+            passed = false;
             //failed
         }
 
