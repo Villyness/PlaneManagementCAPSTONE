991aee4508f942bfc3f60fd7ebfcaf749d4b96e1
Jing <1013263@student.sae.edu.au>
2019-11-17 15:09:58 +1100

    more polish, added cloud animation in tutorial level

---
+++ Assets/Scripts/CloudAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CloudAnim : MonoBehaviour
+{
+    public Transform cloud;
+    public Transform cloudPos;
+    public Transform cloudWaypoint;
+    private float time = 8;
+
+    private void Start()
+    {
+        cloud.position = cloudPos.position;
+    }
+    private void Update()
+    {
+        // creates an animation loop
+        if (cloud.position == cloudPos.position)
+            CloudAnimation();
+    }
+
+    private void CloudAnimation() // for hostess animation in start screen
+    {
+        cloud.DOLocalMoveZ(cloudWaypoint.position.x, time).OnComplete(() => cloud.position = cloudPos.position); // this is why you don't move the hostess in the scene
+    }
+}
